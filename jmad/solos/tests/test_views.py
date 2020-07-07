0b34e3f746299cbe16600e4fb46753b8b9419d2f
Peter Bristow <peterjb73@gmail.com>
2020-07-07 15:37:31 +0100

    Adds Solo model, implemented in index view

--- jmad/solos/tests/test_views.py
+++ jmad/solos/tests/test_views.py
@@ -1,5 +1,7 @@
 from django.test import TestCase, RequestFactory
+from django.db.models.query import QuerySet
 
+from solos.models import Solo
 from solos.views import index
 
 
@@ -10,6 +12,17 @@ class IndexViewTestCase(TestCase):
         # HTTP requests that we can use to test view functions.
         self.factory = RequestFactory()
 
+        self.drum_solo = Solo.objects.create(
+            instrument='drums',
+            artist='Rich',
+            track='Bugle Call Rag'
+        )
+        self.bass_solo = Solo.objects.create(
+            instrument='saxophone',
+            artist='Coltrane',
+            track='Mr. PC'
+        )
+
     def test_index_view_basic(self):
         """
         Test that index view returns a 200 response and uses
@@ -19,3 +32,20 @@ class IndexViewTestCase(TestCase):
         with self.assertTemplateUsed('solos/index.html'):
             response = index(request)
             self.assertEqual(response.status_code, 200)
+
+    def test_index_view_returns_solos(self):
+        """
+        Test that the index view will attempt to return
+        Solos if query parameters exist
+        """
+        # self.client: a sort of dummy browser that can send requests to URLs.
+        response = self.client.get(
+            '/',
+            {'instrument': 'drums'}
+        )
+
+        solos = response.context['solos']
+
+        self.assertIs(type(solos), QuerySet)
+        self.assertEqual(len(solos), 1)
+        self.assertEqual(solos[0].artist, 'Rich')
--- jmad/solos/views.py
+++ jmad/solos/views.py
@@ -1,6 +1,12 @@
 from django.shortcuts import render
 
+from .models import Solo
+
 
 def index(request):
-    context = {}
+    context = {'solos': Solo.objects.filter(
+        instrument=request.GET.get(
+            'instrument', None
+        )
+    )}
     return render(request, 'solos/index.html', context)
